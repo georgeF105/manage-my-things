c042e3949897e44e2c7b6ed4cea23e58a3ed90bb
george francis <georgefrancis105@gmail.com>
2017-07-02 11:30:25 +1200

    Setup nodemon

--- routes/unsorted.js
+++ routes/unsorted.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('unsorted', { title: 'To Be Sorted' });
+  res.render('unsorted', { title: 'To Be Sorted!!' });
 });
 
 module.exports = router;
