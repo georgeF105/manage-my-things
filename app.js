60900fb14311fe3976984377c79942a6c92e9c01
george francis <georgefrancis105@gmail.com>
2017-07-02 11:27:21 +1200

    Make unsorted page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
+var unsorted = require('./routes/unsorted');
 var users = require('./routes/users');
 
 var app = express();
@@ -30,6 +31,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/unsorted', unsorted);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
